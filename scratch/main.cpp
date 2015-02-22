bd539423094d7edbdc9598bfa620e1419d49e441
Sean Parent <sean_parent@mac.com>
2015-02-22 12:12:08 -0800

    Backup

--- scratch/main.cpp
+++ scratch/main.cpp
@@ -1,12 +1,4 @@
 
-#include <utility>
-#include <iostream>
-
-using namespace std;
-
-template <typename I /* unsigned */> size_t count(I x) { size_t r{0}; while (x != 0) { x &= x - 1; ++r; } return r; }
-
-
 
 #if 1
 // GOOD for talk
@@ -14,8 +6,10 @@ template <typename I /* unsigned */> size_t count(I x) { size_t r{0}; while (x !
 #include <iostream>
 #include <memory>
 #include <vector>
+#include <deque>
+#include <thread>
 
-#include <dispatch/dispatch.h>
+//#include <dispatch/dispatch.h>
 
 #include <boost/multiprecision/cpp_int.hpp>
 
@@ -64,6 +58,104 @@ R fibonacci(N n) {
 
 using lock_t = unique_lock<mutex>;
 
+class notification_queue {
+    deque<function<void()>> _q;
+    bool                    _done{false};
+    mutex                   _mutex;
+    condition_variable      _ready;
+    
+public:
+    bool try_pop(function<void()>& x) {
+        lock_t lock{_mutex, try_to_lock};
+        if (!lock || _q.empty()) return false;
+        x = move(_q.front());
+        _q.pop_front();
+        return true;
+    }
+    
+    template<typename F>
+    bool try_push(F&& f) {
+        {
+            lock_t lock{_mutex, try_to_lock};
+            if (!lock) return false;
+            _q.emplace_back(forward<F>(f));
+        }
+        _ready.notify_one();
+        return true;
+    }
+    
+    void done() {
+        {
+            unique_lock<mutex> lock{_mutex};
+            _done = true;
+        }
+        _ready.notify_all();
+    }
+    
+    bool pop(function<void()>& x) {
+        lock_t lock{_mutex};
+        while (_q.empty() && !_done) _ready.wait(lock);
+        if (_q.empty()) return false;
+        x = move(_q.front());
+        _q.pop_front();
+        return true;
+    }
+    
+    template<typename F>
+    void push(F&& f) {
+        {
+            lock_t lock{_mutex};
+            _q.emplace_back(forward<F>(f));
+        }
+        _ready.notify_one();
+    }
+};
+
+class task_system {
+    const unsigned              _count{thread::hardware_concurrency()};
+    vector<thread>              _threads;
+    vector<notification_queue>  _q{_count};
+    atomic<unsigned>            _index{0};
+    
+    void run(unsigned i) {
+        while (true) {
+            function<void()> f;
+            
+            for (unsigned n = 0; n != _count * 32; ++n) {
+                if (_q[(i + n) % _count].try_pop(f)) break;
+            }
+            if (!f && !_q[i].pop(f)) break;
+            
+            f();
+        }
+    }
+    
+public:
+    task_system() {
+        for (unsigned n = 0; n != _count; ++n) {
+            _threads.emplace_back([&, n]{ run(n); });
+        }
+    }
+    
+    ~task_system() {
+        for (auto& e : _q) e.done();
+        for (auto& e : _threads) e.join();
+    }
+    
+    template <typename F>
+    void async_(F&& f) {
+        auto i = _index++;
+        
+        for (unsigned n = 0; n != _count; ++n) {
+            if (_q[(i + n) % _count].try_push(forward<F>(f))) return;
+        }
+        
+        _q[i % _count].push(forward<F>(f));
+    }
+};
+
+task_system _system;
+
 template <typename>
 struct result_of_;
 
@@ -82,6 +174,17 @@ struct shared_base {
     
     virtual ~shared_base() { }
     
+    void set(R&& r) {
+        vector<function<void()>> then;
+        {
+            lock_t lock{_mutex};
+            _r.push_back(move(r));
+            swap(_then, then);
+        }
+        _ready.notify_all();
+        for (const auto& f : then) _system.async_(move(f));
+    }
+    
     template <typename F>
     void then(F&& f) {
         bool resolved{false};
@@ -90,7 +193,7 @@ struct shared_base {
             if (_r.empty()) _then.push_back(forward<F>(f));
             else resolved = true;
         }
-        if (resolved) f();
+        if (resolved) _system.async_(move(f));
     }
     
     const R& get() {
@@ -111,16 +214,8 @@ struct shared<R(Args...)> : shared_base<R> {
     
     template <typename... A>
     void operator()(A&&... args) {
-        auto r = _f(forward<A>(args)...);
+        this->set(_f(forward<A>(args)...));
         _f = nullptr;
-        vector<function<void()>> then;
-        {
-            lock_t lock{this->_mutex};
-            this->_r.push_back(move(r));
-            swap(then, this->_then);
-        }
-        this->_ready.notify_all();
-        for (const auto& f : then) f();
     }
 };
 
@@ -153,6 +248,8 @@ class future {
     const R& get() const { return _p->get(); }
 };
 
+    /* ... */
+
 template<typename R, typename ...Args >
 class packaged_task<R (Args...)> {
     weak_ptr<shared<R(Args...)>> _p;
@@ -179,6 +276,20 @@ auto package(F&& f) -> pair<packaged_task<S>, future<result_of_t_<S>>> {
     return make_pair(packaged_task<S>(p), future<result_of_t_<S>>(p));
 }
 
+
+template <typename F, typename ...Args>
+auto async(F&& f, Args&&... args)
+{
+    using result_type = result_of_t<F (Args...)>;
+    using packaged_type = packaged_task<result_type()>;
+    
+    auto pack = package<result_type()>(bind(forward<F>(f), forward<Args>(args)...));
+    
+    _system.async_(move(get<0>(pack)));
+    return get<1>(pack);
+}
+
+#if 0
 template <typename F, typename ...Args>
 auto async(F&& f, Args&&... args)
 {
@@ -197,25 +308,32 @@ auto async(F&& f, Args&&... args)
     
     return get<1>(pack);
 }
-
-
+#endif
 
 int main() {
-    //auto x = async([]{ return fibonacci<cpp_int>(1'000'000); });
-    packaged_task<int()> f;
     
-    {
-    future<int> x;
-    tie(f, x) = package<int()>([]{ cout << "called" << endl; return 1; });
-        
-    auto a = x.then([](const int& x){ cout << "done 1" << endl; return 1; });
-    auto b = x.then([](const int& x){ cout << x << endl; return 1; });
-    }
+#if 1
+    future<cpp_int> x = async([]{ return fibonacci<cpp_int>(100); });
     
-    //a.get();
-    //b.get();
+    future<cpp_int> y = x.then([](const cpp_int& x){ return cpp_int(x * 2); });
+    future<cpp_int> z = x.then([](const cpp_int& x){ return cpp_int(x / 15); });
+                                                            
+    cout << y.get() << endl;
+    cout << z.get() << endl;
+                                                            
     
-    f();
+#endif
+#if 0
+    
+    auto x = async([]{ return fibonacci<cpp_int>(1'000); });
+    auto a = x.then([](const cpp_int&){ fibonacci<cpp_int>(1'000'000); cout << "done 1" << endl; return 1; });
+    auto b = x.then([](const cpp_int&){ cout << "done 2" << endl; return 2; });
+    
+    a.get();
+    b.get();
+#endif
+                                                
+
 }
 
 #endif
